a16a864e73a23d517fb7155e9df59e79e07a933f
yanxu <yanxu@coocaa.com>
2024-10-10 18:31:32 +0800

    feat: day 2-1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import Links from './links/Links'
 import styles from './navbar.module.css'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <div className={styles.logo}>
+        <Link href='/'>Logo</Link>
+      </div>
       <div>
         <Links />
       </div>

04e66b0d8ec30a051ce4c0a7f681c828b0354d76
yanxu <yanxu@coocaa.com>
2024-10-28 18:30:26 +0800

    feat: update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,13 @@
+import styles from './footer.module.css'
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.logo}>Lama</div>
+      <div className={styles.text}>
+        Lama creative agency copyright &copy; All rights reserved
+      </div>
+    </div>
   )
 }
 
